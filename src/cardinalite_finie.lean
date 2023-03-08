90a3d457c7eda5b3500f5c926503edcc78a104a0
Djamel Ait-Moussa <d.w.aitmoussa@gmail.com>
2023-03-08 03:24:53 +0100

    preuve cardinal produit, cardinalite fonctionne

--- src/cardinalite_finie.lean
+++ src/cardinalite_finie.lean
@@ -152,23 +152,125 @@ end
 
 noncomputable def cardinal (E : Type*) [h : est_fini E] : ℕ := (fini_a_cardinal_fini E).card
 
-universe u
+universes u v
 instance set_to_type {E : Type u} : has_coe (set E) (Type u) := ⟨λ S, {x//S x}⟩
 
-variables (E F : Type u) {n m : ℕ} [h : est_fini E] [h' : est_fini F] (E' E'' : set E)
+variables (E : Type u) (F : Type v) {n m : ℕ} [h : est_fini E] [h' : est_fini F] (E' E'' : set E)
 
 
+protected def my_func (e : E×F): fin (h.majorant*h'.majorant + 1) :=
+fin.mk (h'.majorant * (h.f e.1).val + (h'.f e.2).val)
+      (begin
+        cases nat.decidable_lt 0 h'.majorant with ff tt,
+        {
+          have h₁ : h'.majorant = 0, 
+            cases nat.eq_or_lt_of_not_lt ff with a b,
+              apply eq.symm, exact a,
+              exfalso, exact nat.not_lt_zero h'.majorant b,
+          simp only [h₁, nat.mul_zero, nat.zero_mul, nat.zero_add],
+          have h₂ := (h'.f e.2).property, simp only [h₁] at h₂,
+          have h₃ : 0 < 1, exact nat.zero_lt_succ 0,
+          exact nat.lt_trans h₂ h₃,  
+        },
+        {
+          have h₁ := (h.f e.1).property,
+          cases nat.decidable_lt 0 h.majorant with f t,
+          {
+            exfalso,
+            have h₂ : h.majorant = 0, 
+              cases nat.eq_or_lt_of_not_lt f with a b,
+                apply eq.symm, exact a,
+                exfalso, exact nat.not_lt_zero h.majorant b,
+            simp only [h₂] at h₁,
+            exact nat.not_lt_zero (h.f e.1).val h₁,  
+          },
+          {
+            have h₂ : ∀ n m : ℕ, n < m → n ≤ m.pred,
+              intros n m hnm, cases m,
+                exfalso, exact nat.not_lt_zero _ hnm,
+                rw nat.pred_succ, exact nat.le_of_lt_succ hnm, 
+            have h₃ : (h.f e.1).val ≤ h.majorant.pred, exact h₂ _ _ (h.f e.1).property, 
+            have h₄ : h'.majorant*(h.f e.1).val ≤ h'.majorant*h.majorant.pred := nat.mul_le_mul_of_nonneg_left h₃,
+            have h₅ := nat.add_le_add_right h₄ (h'.f e.2).val,
+            have h₆ := nat.add_lt_add_left (h'.f e.2).property (h'.majorant*h.majorant.pred),
+            have h₇ := nat.lt_of_le_of_lt h₅ h₆, 
+            have h₈ : h.majorant.pred.succ = h.majorant, exact nat.succ_pred_eq_of_pos t,
+            simp only [← h₈] {single_pass := tt},
+            simp only [nat.succ_mul], 
+            apply nat.lt_succ_of_lt,
+            rw nat.mul_comm _ h'.majorant,
+            exact h₇,
+          }
+        }
+      end)
+
+lemma my_func_ident (e : E×F) : (@my_func E F h h' e).val = (h'.majorant * (h.f e.1).val + (h'.f e.2).val)
+  := begin
+    simp only [my_func], 
+  end
 
 @[instance]
 def prod_card_fini : est_fini (E×F) :=
   {
-    majorant := h.majorant*h'.majorant,
-    f := λ ⟨e,f⟩, ⟨h'.majorant * (h.f e).val + (h'.f f).val,
+    majorant := h.majorant*h'.majorant+1,
+    f := @my_func E F h h', 
+    f_inj :=
       begin
-        sorry 
-          
-      end⟩,
-    f_inj := sorry 
+        intros a b hab,
+        simp only [my_func] at hab, 
+        have h₁ := fin.veq_of_eq hab, 
+        simp at h₁, 
+        have h₂ := (h'.f a.2).property,
+        have h₃ := (h'.f b.2).property,
+        have h₄ := h'.majorant * ((h.f a.1).val - (h.f b.1).val) = (h'.f b.2).val - (h'.f a.2).val,
+        have div_eucl_uniq : ∀ n q₁ q₂ r₁ r₂ : ℕ, n*q₁ + r₁ = n*q₂ + r₂ → r₁ < n → r₂ < n → r₁ = r₂, 
+          intro n, intro q₁, revert n, 
+          induction q₁ with k hk; intros n q₂ r₁ r₂ hh₁ hh₂ hh₃, 
+            {rw [nat.mul_zero, nat.zero_add] at hh₁, rw hh₁ at hh₂,
+            cases q₂ with q₂,
+              rw [nat.mul_zero, nat.zero_add] at *, exact hh₁,
+              rw [nat.mul_succ, nat.add_comm _ n, nat.add_assoc, ← nat.add_zero n, nat.add_assoc] at hh₂,
+              have h₅ := nat.lt_of_add_lt_add_left hh₂, 
+              exfalso, exact nat.not_lt_zero _ h₅, 
+            },
+            {
+              rw nat.mul_succ at hh₁, 
+              cases q₂ with q₂,
+                {rw [nat.mul_zero, nat.zero_add] at *, rw← hh₁ at hh₃, 
+                exfalso, rw [nat.add_comm _ n, nat.add_assoc, ← nat.add_zero n, nat.add_assoc] at hh₃, 
+                have h₅ := nat.lt_of_add_lt_add_left hh₃,
+                exact nat.not_lt_zero _ h₅},
+                rw [nat.mul_succ, nat.add_comm (n*k) n, nat.add_assoc] at hh₁,
+                rw [nat.add_comm _ n, nat.add_assoc] at hh₁,
+                have h₆ := nat.add_left_cancel hh₁,
+                exact hk n q₂ r₁ r₂ h₆ hh₂ hh₃, 
+            },
+        have h₅ : (h'.f a.2) = (h'.f b.2) := fin.eq_of_veq (div_eucl_uniq _ _ _ _ _ h₁ h₂ h₃),
+        rw h₅ at h₁, 
+        have h₆ := nat.add_right_cancel h₁, 
+        cases (nat.decidable_lt 0 h'.majorant) with ff tt, 
+        {
+          have h₇ : h'.majorant = 0,
+            cases nat.eq_or_lt_of_not_lt ff with a b,
+            apply eq.symm, assumption,
+            exfalso, exact nat.not_lt_zero _ b,
+          exfalso,
+          simp only [h₇] at h₂,
+          exact nat.not_lt_zero _ h₂,  
+        },
+        {
+          have h₇ : (h'.majorant * (h.f a.1).val)/h'.majorant = (h.f a.1).val := nat.mul_div_cancel_left _ tt,
+          have h₈ := h₇,
+          rw h₆ at h₈, 
+          have h₉ := nat.mul_div_cancel_left (h.f b.1).val tt,
+          rw h₈ at h₉,
+          have h₁₀ := fin.eq_of_veq h₉, 
+          have H₁ := h'.f_inj h₅,
+          have H₂ := h.f_inj h₁₀,
+          cases a, cases b, simp at H₁ H₂,
+          rw [H₁, H₂],
+        }
+      end
   }
 
 @[instance]
@@ -187,5 +289,8 @@ theorem card_of_func [h : est_fini E] [h' : est_fini F]
   : cardinal (E→F) = cardinal F ^ cardinal E :=
   sorry
 
+theorem eq_card_of_idempotents [h : est_fini E] [h' : est_fini F] (bij : bijections E F) 
+  : cardinal E = cardinal F := 
+  sorry
 
 end -- section utilisant classical
